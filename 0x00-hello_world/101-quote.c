474d10eb1d3979037fa1ef65bd634a02c6c5f288
Razak-Gawusu <gawusularbil@gmail.com>
2022-12-08 12:55:31 -0800

    more commits

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+/**
+ *  * main - main block
+ *   * Return: 1
+ *   */
+int main(void)
+{
+		int t = strlen(
+						"and that piece of art is useful\"- Dora Korpar, 2015-10-19\n");
+
+			write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", t);
+
+				return (1);
+}
